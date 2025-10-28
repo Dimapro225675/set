fb4928784bf071cbc2a510c3e8fc70eb0b0fb789
Dimapro225675 <dimasirota42@gmail.com>
2025-10-28 08:17:10 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# set1 = {1, 2, 3, 4 , 5}
+# print(set1)
+
+
+# set1 = {"банан", "ананас", "яблоко", "апельсин", "манго"}
+# user_input = input("Ыыдите фрукт: ")
+# print(user_input in set1)
+
+
+# set1 = {1, 2, 3, 4, 5}
+# set1.add(6)
+# print(set1)
+
+
+# set1 = {1, 2, 3, 4, 5}
+# set1.remove(3)
+# print(set1)
+
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {6, 7, 8, 9, 10}
+#
+# union1 = set1.union(set2)
+# print(union1)
+
+
+# set1 = {1, 2, 3, 4, 5, 6}
+# set2 = {5, 6, 7, 8, 9, 10}
+#
+# intersection1 = set1.intersection(set2)
+# print(intersection1)
+
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {1, 2, 5}
+#
+# difference1 = set1 - set2
+# print(difference1)
+
+
+# set1 = {1, 2, 4, 5, 6}
+# set2 = {1, 2, 3, 4, 5}
+#
+# symmetric_difference1 = set1 ^ set2s
+# print(symmetric_difference1)
+
+
+# set1 = {1, 2, 3, 4, 5, 6}
+# set2 = {1, 2, 3}
+#
+# result = set2.issubset(set1)
+# print(result)
+
+
+
+# set1 = {1, 2, 4, 5, 6, 7}
+# for element in set1:
+#     print(element)
